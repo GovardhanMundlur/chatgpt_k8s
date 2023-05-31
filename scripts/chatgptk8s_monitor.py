4452465f73054ec2fc4ddc2636ad70cdac45fd1c
GovardhanMundlur <gv3br10ee032@gmail.com>
2023-05-30 22:43:17 -0400

    added condition to filter the waiting containers

--- scripts/chatgptk8s_monitor.py
+++ scripts/chatgptk8s_monitor.py
@@ -2,12 +2,13 @@
 import requests
 import json
 from kubernetes import client, config
+from kubernetes.client import configuration 
 
 config.load_kube_config()
+configuration.assert_hostname = False
+k8sapi = client.CoreV1Api()
 
-v1 = client.CoreV1Api()
-
-resp_obj = v1.list_pod_for_all_namespaces(
+resp_obj = k8sapi.list_pod_for_all_namespaces(
 	watch=False,
 	_preload_content=False
 	)
@@ -15,4 +16,11 @@ pods_all = json.loads(resp_obj.data)
 
 
 for pod in pods_all["items"]:
-	print(pod["metadata"]["name"])
+	pod_name  = pod["metadata"]["name"]
+	pod_ns = pod["metadata"]["namespace"] 
+	pod_details = json.loads((k8sapi.read_namespaced_pod(name=pod_name, namespace=pod_ns, _preload_content=False)).data)
+	for container in pod_details["status"]["containerStatuses"]:
+		if "waiting" in container["state"]:
+			pod_log = json.loads((k8sapi.read_namespaced_pod_log(name=pod_name, namespace=pod_ns, _preload_content=False)).data)
+			#print(pod_log)
+	print("-------")
